d141991ce8de0945ae09d2d6536d0e44e87bf6d1
Joshua Lookshin <joshdlooks2@gmail.com>
2026-01-30 12:18:11 -0500

    - Added in the option to use proxies. - Changed so that usernames would be retried until they received a positive response code and were told that the name was no longer available. - Optimized some code

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import os
 import random as rd
 import tempfile
 import shutil
+from operator import index
 from pathlib import Path
 from urllib.parse import uses_netloc
 # Third party
@@ -14,31 +15,39 @@ from exceptiongroup import catch
 from selenium.common import TimeoutException
 from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 
+"""
+
+"""
 
 ##--FUNCTIONS--
 def import_user_config():
     """
         Imports data from the user configuration file and the list of names.
-       :return: list_of_names, email, password, min_delay, max_delay
+       :return: list_of_names, email, password, min_delay, max_delay, rate_limit_delay
     """
-    list_of_names, email, password, min_delay, max_delay = [], "", "", 0, 0
-
-    with open("../Config/user_config", "r") as f:
+    with (open("../Config/user_config", "r") as f):
         config = json.load(f)
-        email, password, min_delay, max_delay = config["Email"], config["Password"], config["Minimum_Delay"], config["Maximum_Delay"]
-
     with open("../Config/name_list", "r") as f:
-        list = json.load(f)
-        list_of_names = [name for name in list]
-
+        dict = json.load(f)
+        list_of_names = dict.keys()
     print(f"Imported {len(list_of_names)} names.")
-    return list_of_names, email, password, min_delay, max_delay
+    print(f"min_delay: {config['Minimum_Delay']}, max_delay: {config['Maximum_Delay']}, rate_limit_delay: {config['Rate_Limit_Delay']}")
+
+    return (
+        list_of_names,
+        config['Email'],
+        config['Password'],
+        config['Minimum_Delay'],
+        config['Maximum_Delay'],
+        config['Rate_Limit_Delay'],
+        config['Proxy'],
+    )
 
 
 def start_chrome_with_clean_profile():
@@ -48,7 +57,8 @@ def start_chrome_with_clean_profile():
     profile_dir = tempfile.mkdtemp()
     opts = Options()
     opts.add_argument(f"--user-data-dir={profile_dir}")
-    opts.add_experimental_option("detach", True)
+    opts.add_argument("--disable-webrtc")
+    opts.add_argument("--disable-blink-features=AutomationControlled")
     driver = webdriver.Chrome(options=opts)
     return driver, profile_dir
 
@@ -64,6 +74,32 @@ def start_chrome_with_normal_profile():
     driver = webdriver.Chrome(options=opts)
     return driver, profile_dir
 
+def start_chrome_with_proxy(proxy):
+    """
+    Opens a browser instance with a clean cache and cookies, and a proxy.
+    :param proxy: proxy formatted as server:port:username:password
+    """
+    profile_dir = tempfile.mkdtemp()
+
+    chrome_options = Options()
+    chrome_options.add_argument(f"--user-data-dir={profile_dir}")
+    chrome_options.add_argument("--disable-webrtc")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+
+    seleniumwire_options = {
+        "proxy": {
+            "http": f"http://{proxy}",
+            "https": f"http://{proxy}" ,
+            "no_proxy": "localhost,127.0.0.1"
+        }
+    }
+
+    driver = webdriver.Chrome(
+        options=chrome_options,
+        seleniumwire_options=seleniumwire_options
+    )
+
+    return driver, profile_dir
 
 
 def login(email, password):
@@ -71,7 +107,7 @@ def login(email, password):
     if len(element) == 0:
         try:
             # Finds and clicks "login with email" button
-            WebDriverWait(driver, 10).until(
+            WebDriverWait(driver, 2).until(
                 EC.presence_of_element_located((By.XPATH, '//*[@id="login-form"]/div[1]/div/button'))
             )
             login_with_email_button = driver.find_element(By.XPATH, '//*[@id="login-form"]/div[1]/div/button')
@@ -79,7 +115,7 @@ def login(email, password):
 
             # Enters email and password
             time.sleep(1)
-            WebDriverWait(driver, 10).until(
+            WebDriverWait(driver, 3).until(
                 EC.presence_of_element_located((By.ID, "identifier"))
                 and
                 EC.presence_of_element_located((By.ID, "password"))
@@ -91,84 +127,135 @@ def login(email, password):
         except:
             print("Error: Couldn't find login buttons or inputs.")
 
-    # Finds and clicks "change username" button
-    time.sleep(2)
-    try:
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button[aria-label='Change username']"))
-        )
-        change_username_button = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Change username']")
-        change_username_button.click()
-    except:
-        print("Error: Couldn't find 'Change Username' button.")
-
-
 
 total_name_change_attempts = 0
 
-def change_Name(list_of_names, min_delay, max_delay):
+def change_Name(list_of_names, min_delay, max_delay, rate_limit_delay):
     global total_name_change_attempts
-    rate_limit_delay = 7
-    while True:
-        for name in list_of_names:
-            # Finds input element and sends username
-            WebDriverWait(driver, 120).until(
-                EC.presence_of_element_located((By.XPATH, '//*[@id="modal-0e4d3ecc-1c04-4277-98b8-47b5d8a01a21"]'))
-            )
+
+    index = 0
+    while index <= len(list_of_names):
+        name = list_of_names[index]
+        # Finds input element and sends username
+        WebDriverWait(driver, 120).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="modal-0e4d3ecc-1c04-4277-98b8-47b5d8a01a21"]'))
+        )
+        try:
             input_element = driver.find_element(By.XPATH, '//*[@id="modal-0e4d3ecc-1c04-4277-98b8-47b5d8a01a21"]')
-            try:
-                input_element.clear()
-                if input_element.get_attribute("value") == "":
-                    input_element.send_keys(name)
-            except:
-                pass
-            # Checks if captcha is visible and, if it is, waits until the captcha is filled out.
-            captcha = driver.find_elements(By.XPATH, '//*[@id="turnstile-modal-title"]')
-            if captcha:
-                WebDriverWait(driver, 120).until_not(
-                    EC.visibility_of_element_located((By.XPATH, '//*[@id="turnstile-modal-title"]'))
+            submit_button = driver.find_element(By.XPATH, '//*[@id="game-profiles"]/div/div[2]/div/form/button')
+            input_element.clear()
+            WebDriverWait(driver, 10).until(
+                lambda a: input_element.get_attribute("value") == ""
+            )
+            for letter in name:
+                input_element.send_keys(letter)
+                time.sleep(rd.uniform(0.1, 0.3))
+                submit_button.click()
+        except:
+            pass
+
+        # Checks if captcha is visible and, if it is, waits until the captcha is filled out.
+        try:
+            captcha_element = driver.find_element(By.XPATH, '//span[normalize-space()="Verify you are human"]')
+            if captcha_element:
+                WebDriverWait(driver, 120).until(
+                    EC.invisibility_of_element_located(By.XPATH, '//span[normalize-space()="Verify you are human"]')
                 )
+                time.sleep(7)
+        except:
+            pass
 
 
-            # Tries to get response code
-            total_name_change_attempts += 1
-            response_code = 0
+        # Tries to get response code
+        total_name_change_attempts += 1
+        response_code = 0
+        while response_code == 0:
             try:
                 response_code = driver.requests[-1].response.status_code
             except:
-                response_code = "No response code found."
-
-            print(f"Attempted to change name to: {name}.    |   Response code: {response_code}  |   Attempt {total_name_change_attempts}.   |   Current rate limit delay: {rate_limit_delay}")
-
-            # Sleeps if response code was unsuccessful.
-            # sleep time adapts according to previous response codes.
-            if response_code >= 400:
-                rate_limit_delay += 2
-                time.sleep(rate_limit_delay)
-            elif response_code == 200 and rate_limit_delay >= 1:
-                rate_limit_delay -= 1
-
-            # Exits loop after a certain amount of attempts.
-            if (total_name_change_attempts % 100000 == 0):
-                return
+                pass
 
 
-            # Sleeps for random durations according to the delays set in the user configuration file.
-            time.sleep(rd.randint(min_delay, max_delay))
+
+        # Sleeps if response code was unsuccessful.
+        # sleep time adapts according to previous response codes.
+        random_delay = rd.uniform(min_delay, max_delay)
+        total_delay = 0
+        if response_code >= 400:
+            total_delay += (rate_limit_delay)
+            print_info(name, response_code, total_name_change_attempts, total_delay)
+            time.sleep(30)
+        elif 300 < response_code < 400:
+            total_delay += random_delay
+            print_info(name, response_code, total_name_change_attempts, total_delay)
+            time.sleep(random_delay)
+        elif response_code == 200:
+            total_delay += random_delay
+            print_info(name, response_code, total_name_change_attempts, total_delay)
+            time.sleep(random_delay)
+            if is_still_available():
+                click_submit_button()
+            else:
+                index += 1
+        else:
+            total_delay += random_delay
+            print_info(name, response_code, total_name_change_attempts, total_delay)
+            time.sleep(random_delay)
+
+def is_still_available():
+    try:
+        status_element = driver.find_element(By.XPATH, '//*[@id="game-profiles"]/div/div[2]/div/form/div[2]/p')
+        status = status_element.text
+        print(f"Status: {status}")
+        if status != "Username is already in use":
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def click_submit_button():
+    try:
+        submit_button = driver.find_element(By.XPATH, '//*[@id="game-profiles"]/div/div[2]/div/form/button')
+        submit_button.click()
+    except:
+        pass
+
+def get_ip(driver):
+    driver.get("https://api.ipify.org")
+    try:
+        return driver.page_source.split('>')[6].split('<')[0]
+    except:
+        return "Error: Couldn't get IP."
+
+def print_info(name, response_code, total_name_change_attempts, total_delay):
+    print(
+        f"Attempted to change name to: {name}.    |   "
+        f"Response code: {response_code}  |   "
+        f"Attempt {total_name_change_attempts}.   |   "
+        f"Delay: {total_delay}")
 
 
 ##---MAIN---
 
 # Imports data
 URL = "https://accounts.hytale.com/profiles"
-list_of_names, email, password, min_delay, max_delay = import_user_config()
+list_of_names, email, password, min_delay, max_delay, rate_limit_delay, proxy = import_user_config()
 
 while True:
     # Opens browser and loads website
     service  = Service(executable_path="chromedriver.exe")
-    driver, prof = start_chrome_with_normal_profile()
+    if proxy != "":
+        driver, prof = start_chrome_with_proxy(proxy)
+    else:
+        driver, prof = start_chrome_with_normal_profile()
+
+    # Gets the IP being used so you can ensure that the proxy worked
+    print(f"IP: {get_ip(driver)}")
+
+    # Opens website
     driver.get(URL)
-    time.sleep(3)
+    time.sleep(1)
 
     # Tries to login
     try:
@@ -181,7 +268,7 @@ while True:
 
     # Tries to change username
     try:
-        change_Name(list_of_names, min_delay, max_delay)
+        change_Name(list_of_names, min_delay, max_delay, rate_limit_delay)
     except:
         print("Error: change_name function didn't work.")
 
